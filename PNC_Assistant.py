fee344a8af0876f645b25b2847d2c3ce29db38f2
Shuang0420 <sxu1@andrew.cmu.edu>
2017-05-04 13:49:26 -0400

    Modify balance and transaction intent

--- PNC_Assistant.py
+++ PNC_Assistant.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, render_template
 from flask_ask import Ask, statement, question, session
 import json
 import requests
 import time
 import unidecode
-
+#import web
 
 # sample username and password
 USERNAME = 'mayduncan323'
@@ -12,6 +13,8 @@ PASSWORD = 'mayduncan323'
 URL = "https://nginx0.pncapix.com"
 version = "/v1.0.0"
 header_dict = {"Content-Type": "application/json", "Accept": "application/json", 'Authorization': "Bearer efa92a43-be7b-32ef-a6df-ef1831d4d9ca"}
+account_types = {"STANDARD_SAVINGS": 1, "STANDARD_CHECKING": 2, "PNC_CORE_VISA_CREDIT_CARD": 6, "VIRTUAL_WALLET_SPEND": 15, "VIRTUAL_WALLET_RESERVE": 16,
+"VIRTUAL_WALLET_GROWTH": 17}
 
 pnc_api_token = ''
 
@@ -19,18 +22,6 @@ pnc_api_token = ''
 app = Flask(__name__)
 # give basic endpoint, can be flask skill/program endpoint
 ask = Ask(app, "/pnc_assistant")
-
-# check balance
-def getBalance():
-    pass
-
-
-
-# track transaction
-def checkTransaction():
-    pass
-
-
 
 
 # get answer to frequent asked questions
@@ -41,18 +32,53 @@ def getFAQ():
 
 
 # set home url path
-@app.route('/')
+@app.route('/pnc_assistant')
 def homepage():
-    return 'hi there, how ya doin?'
+    return 'welcome to pnc assistant'
 
 
 @ask.launch
 def start_skill():
     # it will say
-    welcome_message = 'Hello there, would you like the news?'
+    welcome_message = 'Hello there, what can I do for you?'
     # question expect response
     return question(welcome_message)
 
+
+
+@ask.intent("BalanceIntent", mapping={'account_type': 'AccountType'})
+def checkBalance(account_type):
+    # bal = web.getAccount()
+    bal = 999
+    # session.attributes['query_type'] = 'balance'
+    if account_type:
+        bal = 111
+    else:
+        account_type = 'standard checking'
+    stat = ' '.join(['Your', account_type, 'balance is'])
+    return statement(' '.join([stat, ' . ', str(bal), ' . ', 'anything else I can help you']))
+
+
+@ask.intent("TransactionIntent", mapping={'time_span': 'TimeSpan'})
+def checkTransaction(time_span):
+    # bal = web.getAccount()
+    bal = 999
+    # session.attributes['query_type'] = 'balance'
+    if time_span:
+        bal = 111
+    else:
+        time_span = 'week'
+    stat = ' '.join(['You spent . ', str(bal), 'last',time_span, ' . ', 'anything else I can help you'])
+    print bal, time_span, stat
+    return statement(stat)
+
+
+
+
+
+@ask.intent("FAQIntent")
+def FAQ():
+    pass
 
 # handle user input yes or no response
 # user input is intent
@@ -70,6 +96,30 @@ def no_intent():
     bye_text = 'I am not sure why you asked me to run then, but okay... bye'
     return statement(bye_text)
 
+
+
+def alexa_stop_cancel():
+  if session.new:
+    return statement("how can I help you")
+  else:
+    return statement("")
+
+# Handle the AMAZON.StopIntent intent.
+@ask.intent('AMAZON.StopIntent')
+def alexa_stop():
+  return alexa_stop_cancel()
+
+
+# Handle the AMAZON.CancelIntent intent.
+@ask.intent('AMAZON.CancelIntent')
+def alexa_cancel():
+  return alexa_stop_cancel()
+
+
+# Handle the AMAZON.NoIntent intent.
+@ask.intent('AMAZON.NoIntent')
+def alexa_no():
+  return alexa_stop_cancel()
 
 
 def login(username, password):
